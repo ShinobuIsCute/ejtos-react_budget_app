3d0c2efa3ebab400e9f8a490a88ad5f94034f952
Pranav <pransorigami@gmail.com>
2024-05-29 02:46:05 -0400

    Changes to final assignment

---
+++ src/components/Currency.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Currency = () => {
+  const {dispatch } = useContext(AppContext);
+
+    const changeCurrency = (val)=>{
+            dispatch({
+                type: 'CHG_CURRENCY',
+                payload: val,
+            })
+    }
+
+    return (
+		<div className='btn btn-success dropdown-toggle'>Currency
+      <select className='btn btn-success dropdown-toggle' name="Currency" id="Currency" value={currency} onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#33FF49', color:'white'}}>
+        <option style={{color:'black'}} value="£">£ Pound</option>
+        <option style={{color:'black'}} value="$">$ Dollar</option>
+        <option style={{color:'black'}} value="€">€ Euro</option>
+        <option style={{color:'black'}} value="₹">₹ Rupee</option>
+      </select>	
+    </div>
+	);
+};
+
+export default Currency;
